8da3aed0b581c3840e339d78c2458964a0a39451
carterols <cjolsen06@gmail.com>
2020-03-28 22:12:33 -0700

    Updates

---
+++ precache-manifest.e0945a1fe61815cb6b741abe6efedffb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bde27b7e4ee69115cc918cb4e83df33",
+    "url": "/cod-warzone-stats/index.html"
+  },
+  {
+    "revision": "21f76b62f18972614cb9",
+    "url": "/cod-warzone-stats/static/css/main.032085fa.chunk.css"
+  },
+  {
+    "revision": "a89786dd99c27e84a327",
+    "url": "/cod-warzone-stats/static/js/2.e0d2509d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cod-warzone-stats/static/js/2.e0d2509d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f76b62f18972614cb9",
+    "url": "/cod-warzone-stats/static/js/main.4c72ab57.chunk.js"
+  },
+  {
+    "revision": "ab523fb14d5591148891",
+    "url": "/cod-warzone-stats/static/js/runtime-main.7d2e8644.js"
+  },
+  {
+    "revision": "355d0860697e144d5983f0ad4061a4ef",
+    "url": "/cod-warzone-stats/static/media/battle-net.355d0860.svg"
+  },
+  {
+    "revision": "70872efd136075054718a74e5697b6c1",
+    "url": "/cod-warzone-stats/static/media/psn.70872efd.svg"
+  },
+  {
+    "revision": "faa28b5a73aef139877dd5149d95d9ca",
+    "url": "/cod-warzone-stats/static/media/xbox.faa28b5a.svg"
+  }
+]);
