19454eff17e11169ffea60beed57f9ee7b79d495
carterols <cjolsen06@gmail.com>
2020-04-14 13:16:34 -0700

    updated client code to use server

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const COD_API_BASE_URL = "https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/";
-const COD_LOGIN = "https://profile.callofduty.com/cod/login";
+const COD_API_BASE_URL = "https://pacific-badlands-14289.herokuapp.com";
 
 export default {
     getUser: async (user, platform) => {
         const uid = user.replace("#", "%23");
-        const url = `${COD_API_BASE_URL + platform}/gamer/${uid}/profile/type/mp`;
+        const url = `${COD_API_BASE_URL}/stats/platform/${platform}/gamer/${uid}/profile`;
         const res = await axios.get(url);
 
         if (res.data.status !== 'error') {
@@ -17,7 +16,7 @@ export default {
     },
     getFriendsStats: async (user, platform) => {
         const uid = user.replace("#", "%23");
-        const url = `${COD_API_BASE_URL + platform}/gamer/${uid}/profile/friends/type/mp`;
+        const url = `${COD_API_BASE_URL}/stats/platform/${platform}/gamer/${uid}/friends`;
         const res = await axios.get(url);
 
         if (res.data.status !== 'error') {
@@ -28,7 +27,7 @@ export default {
     },
     getRecentMatches: async (user, platform) => {
         const uid = user.replace('#', '%23');
-        const res = await axios.get(`https://pacific-badlands-14289.herokuapp.com/matches/platform/${platform}/user/${uid}`);
+        const res = await axios.get(`${COD_API_BASE_URL}/matches/platform/${platform}/user/${uid}`);
         return res.data;
     }
 }
